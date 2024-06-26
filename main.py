db45232b994c2effa9df1bf768d4b0795739a386
ksg9482 <ksg930523@gmail.com>
2024-06-27 00:57:20 +0900

    add keyword arguments

--- main.py
+++ main.py
@@ -11,12 +11,10 @@ async def app_lifespan(app: FastAPI):
     yield
 
 
-app = FastAPI(
-    lifespan=app_lifespan
-)
+app = FastAPI(lifespan=app_lifespan)
 
-app.include_router(post_router)
+app.include_router(router=post_router)
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
+    uvicorn.run(app="main:app", host="0.0.0.0", port=8080, reload=True)
--- src/routers/post.py
+++ src/routers/post.py
@@ -13,7 +13,11 @@ router = APIRouter(prefix="/posts", tags=["posts"])
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_post(post: Post, db: AsyncSession = Depends(Base.get_db)) -> int:
-    new_post = PostModel(author=post.author, title=post.title, content=post.content)
+    new_post = PostModel(
+        author=post.author, 
+        title=post.title, 
+        content=post.content
+    )
     db.add(new_post)
     await db.commit()
     await db.refresh(new_post)
@@ -36,6 +40,9 @@ async def get_post(post_id: int, db: AsyncSession = Depends(Base.get_db)) -> Pos
     post = result.scalars().first()
     
     if post == None:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Post not found")
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, 
+            detail="Post not found"
+        )
     
     return post
