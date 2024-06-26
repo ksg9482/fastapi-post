d965850ea9dc958321d69c122d0db4fc00046965
ksg9482 <ksg930523@gmail.com>
2024-06-27 00:33:15 +0900

    add type hint

--- src/routers/post.py
+++ src/routers/post.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -11,8 +11,8 @@ from src.database import Base
 
 router = APIRouter(prefix="/posts", tags=["posts"])
 
-@router.post("/")
-async def create_post(post: Post, db: AsyncSession = Depends(Base.get_db)):
+@router.post("/", status_code=status.HTTP_201_CREATED)
+async def create_post(post: Post, db: AsyncSession = Depends(Base.get_db)) -> int:
     new_post = PostModel(author=post.author, title=post.title, content=post.content)
     db.add(new_post)
     await db.commit()
@@ -20,15 +20,15 @@ async def create_post(post: Post, db: AsyncSession = Depends(Base.get_db)):
     return new_post.id
 
 
-@router.get("/", response_model=List[Post])
-async def get_post_list(db: AsyncSession = Depends(Base.get_db)):
+@router.get("/", response_model=List[Post], status_code=status.HTTP_200_OK)
+async def get_post_list(db: AsyncSession = Depends(Base.get_db)) -> List[Post]:
     result = await db.execute(select(PostModel))
     posts = result.scalars()._allrows()
     return posts
 
 
-@router.get("/{post_id}", response_model=Post)
-async def get_post(post_id: int, db: AsyncSession = Depends(Base.get_db)):
+@router.get("/{post_id}", response_model=Post, status_code=status.HTTP_200_OK)
+async def get_post(post_id: int, db: AsyncSession = Depends(Base.get_db)) -> Post:
     result = await db.execute(
                 select(PostModel)
                 .where(PostModel.id == post_id)
