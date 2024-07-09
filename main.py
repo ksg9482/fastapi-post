9cdd00a74d89b32c7c91c1f5513ecac8a17de62f
ksg9482 <ksg930523@gmail.com>
2024-07-09 22:25:51 +0900

    레이어드 아키텍처 적용에 따라 import 변경

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import uvicorn
 from fastapi import FastAPI
 
 from src.database import db_init
-from src.routers.post import router as post_router
-from src.routers.user import router as user_router
+from src.API.post import router as post_router
+from src.API.user import router as user_router
 
 
 app = FastAPI(lifespan=db_init)
