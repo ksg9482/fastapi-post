3c206c359ed4e86d8c32ecc5d8b00cd95b2642fc
ksg9482 <ksg930523@gmail.com>
2024-07-28 23:23:20 +0900

    e2e 테스트 작성

---
+++ tests/e2e/test_comment_e2e.py
@@ -0,0 +1,259 @@
+from fastapi.testclient import TestClient
+
+from fastapi.testclient import TestClient
+import pytest
+
+from main import app
+
+
+@pytest.fixture
+def client():
+    with TestClient(app) as client:
+        client.post(
+            "/users",
+            json={
+                "nickname": "test_user",
+                "password": "Test_password",
+            },
+        )
+
+        client.post(
+            "/users/login",
+            json={
+                "nickname": "test_user",
+                "password": "Test_password",
+            },
+        )
+
+        # 명시해야 하나??
+        # client.cookies = {
+        #     "session_id": response.json()['session_id']
+        # }
+
+        # 미리 post 생성
+        client.post(
+            "/posts",
+            json={
+                "title": "test_title",
+                "content": "test_content",
+            },
+        )
+        yield client
+
+
+def test_create_comment_ok(client: TestClient):
+    response = client.post(
+        "/comments/1",
+        json={
+            "content": "test_comment",
+        },
+    )
+    assert response.status_code == 201
+    assert response.json()["content"] == "test_comment"
+
+
+def test_create_comment_invalid_params(client: TestClient):
+    response = client.post(
+        url="/comments/1",
+        json={
+            "content": None,
+        },
+    )
+    assert response.status_code == 422
+
+
+def test_create_comment_post_missing_field(client: TestClient):
+    response = client.post(
+        url="/comments/1",
+        json={
+            "content": None,
+        },
+    )
+    assert response.status_code == 422
+
+
+def test_create_comment_post_not_exists(client: TestClient):
+    response = client.post(
+        url="/comments/9999",
+        json={
+            "content": "test_comment",
+        },
+    )
+    assert response.status_code == 400
+    assert response.json()["detail"] == "존재하지 않는 포스트입니다"
+
+
+# 페이지네이션 해야함
+def test_comment_list_by_post_ok(client: TestClient):
+    client.post(
+        "/comments/1",
+        json={
+            "content": "test_comment_1",
+        },
+    )
+
+    client.post(
+        "/comments/1",
+        json={
+            "content": "test_comment_2",
+        },
+    )
+    response = client.get("/comments/by_post/1")
+
+    assert response.status_code == 200
+    assert len(response.json()["comments"]) == 2
+
+
+def test_comment_list_by_post_empty_ok(client: TestClient):
+    response = client.get("/comments/by_post/1")
+
+    assert response.status_code == 200
+    assert len(response.json()["comments"]) == 0
+
+
+# 다른 유저가 넣은거 처리 해야함
+def test_comment_list_by_user_ok(client: TestClient):
+    client.post(
+        "/comments/1",
+        json={
+            "content": "test_comment_1",
+        },
+    )
+
+    client.post(
+        "/comments/1",
+        json={
+            "content": "test_comment_2",
+        },
+    )
+    response = client.get("/comments/by_user/1")
+
+    assert response.status_code == 200
+    assert len(response.json()["comments"]) == 2
+
+
+def test_comment_list_by_user_empty_ok(client: TestClient):
+    response = client.get("/comments/by_user/1")
+
+    assert response.status_code == 200
+    assert len(response.json()["comments"]) == 0
+
+
+# 없는거 로직을 넣어야 하나?
+# def test_comment_list_fail(client: TestClient):
+#     response = client.get(
+#         "/posts"
+#     )
+#     assert response.status_code == 500
+
+
+def test_comment_patch_ok(client: TestClient):
+    client.post(
+        url="/comments/1",
+        json={
+            "content": "test_comment_1",
+        },
+    )
+
+    response = client.patch(url="/comments/1", json={"content": "test_comment_1_edit"})
+    assert response.status_code == 204
+
+    comment_response = client.get("/comments/by_post/1")
+    assert comment_response.status_code == 200
+    assert comment_response.json()["comments"][0]["content"] == "test_comment_1_edit"
+
+
+def test_comment_patch_not_exists(client: TestClient):
+    response = client.patch(url="/comments/1", json={"content": "test_comment_1_edit"})
+
+    assert response.status_code == 400
+    assert response.json()["detail"] == "존재하지 않는 코멘트입니다"
+
+
+def test_comment_patch_invalid_author(client: TestClient):
+    client.post(
+        url="/comments/1",
+        json={
+            "content": "test_comment_1",
+        },
+    )
+
+    # 다른 아이디로 수정 시도. 세션아이디 변경 됨
+    client.post(
+        "/users",
+        json={
+            "nickname": "test_user_2",
+            "password": "Test_password",
+        },
+    )
+
+    client.post(
+        "/users/login",
+        json={
+            "nickname": "test_user_2",
+            "password": "Test_password",
+        },
+    )
+
+    # 쿠키에 이미 변경된 아이디가 들어있음. 명시로 다시 넣어주는게 좋은가?
+
+    response = client.patch(url="/comments/1", json={"content": "test_content_1_edit"})
+
+    assert response.status_code == 403
+    assert response.json()["detail"] == "작성자만 수정할 수 있습니다"
+
+
+def test_comment_delete_ok(client: TestClient):
+    client.post(
+        url="/comments/1",
+        json={
+            "content": "test_comment_1",
+        },
+    )
+
+    delete_response = client.delete(url="/comments/1")
+    assert delete_response.status_code == 204
+
+    comment_response = client.get("/comments/by_post/1")
+    assert comment_response.status_code == 200
+    assert comment_response.json()["comments"] == []
+
+
+def test_comment_put_not_exists(client: TestClient):
+    response = client.delete(url="/comments/1")
+
+    assert response.status_code == 400
+    assert response.json()["detail"] == "존재하지 않는 코멘트입니다"
+
+
+def test_comment_delete_invalid_author(client: TestClient):
+    client.post(
+        "/comments/1",
+        json={
+            "content": "test_comment_1",
+        },
+    )
+
+    # 다른 아이디로 삭제 시도. 세션아이디 변경 됨
+    client.post(
+        "/users",
+        json={
+            "nickname": "test_user_2",
+            "password": "Test_password",
+        },
+    )
+
+    client.post(
+        "/users/login",
+        json={
+            "nickname": "test_user_2",
+            "password": "Test_password",
+        },
+    )
+
+    # 쿠키에 이미 변경된 아이디가 들어있음. 명시로 다시 넣어주는게 좋은가?
+
+    response = client.delete(url="/comments/1")
+
+    assert response.status_code == 403
+    assert response.json()["detail"] == "작성자만 삭제할 수 있습니다"
---
+++ tests/e2e/test_user_e2e.py
@@ -0,0 +1,178 @@
+from fastapi.testclient import TestClient
+
+from fastapi.testclient import TestClient
+
+from main import app
+
+
+def test_signup_user_ok():
+    with TestClient(app) as client:
+        response = client.post(
+            "/users",
+            json={
+                "nickname": "test_user",
+                "password": "Test_password",
+            },
+        )
+        assert response.status_code == 201
+        assert response.json() == {"id": 1, "nickname": "test_user"}
+
+
+def test_signup_user_not_upper():
+    with TestClient(app) as client:
+        response = client.post(
+            "/users",
+            json={
+                "nickname": "test_user",
+                "password": "test_password",
+            },
+        )
+        assert response.status_code == 400
+        assert (
+            response.json()["detail"] == "문자열에 대문자가 하나 이상 포함되어야 합니다"
+        )
+
+
+def test_signup_user_invalid_params():
+    with TestClient(app) as client:
+        response = client.post(
+            "/users",
+            json={
+                "nickname": "",
+                "password": "",
+            },
+        )
+        assert response.status_code == 422
+        # assert response.json()['detail'] == "문자열에 대문자가 하나 이상 포함되어야 합니다"
+
+
+def test_signup_user_duplicate():
+    with TestClient(app) as client:
+        client.post(
+            "/users",
+            json={
+                "nickname": "test_user",
+                "password": "Test_password",
+            },
+        )
+
+        response = client.post(
+            "/users",
+            json={
+                "nickname": "test_user",
+                "password": "Test_password",
+            },
+        )
+        assert response.status_code == 400
+        assert response.json()["detail"] == "이미 가입한 유저입니다"
+
+
+def test_login_ok():
+    with TestClient(app) as client:
+        client.post(
+            "/users",
+            json={
+                "nickname": "test_user",
+                "password": "Test_password",
+            },
+        )
+
+        response = client.post(
+            "/users/login",
+            json={
+                "nickname": "test_user",
+                "password": "Test_password",
+            },
+        )
+        assert response.status_code == 200
+        assert response.json()["session_id"]
+
+
+def test_login_invalid_nikcname():
+    with TestClient(app) as client:
+        client.post(
+            "/users",
+            json={
+                "nickname": "test_user",
+                "password": "Test_password",
+            },
+        )
+
+        response = client.post(
+            "/users/login",
+            json={
+                "nickname": "invalid_user",
+                "password": "Test_password",
+            },
+        )
+        assert response.status_code == 400
+        assert response.json()["detail"] == "존재하지 않는 유저입니다"
+
+
+def test_login_invalid_password():
+    with TestClient(app) as client:
+        client.post(
+            "/users",
+            json={
+                "nickname": "test_user",
+                "password": "Test_password",
+            },
+        )
+
+        response = client.post(
+            "/users/login",
+            json={
+                "nickname": "test_user",
+                "password": "invalid_password",
+            },
+        )
+        assert response.status_code == 400
+        assert response.json()["detail"] == "잘못된 비밀번호입니다"
+
+
+def test_logout_ok():
+    with TestClient(app) as client:
+        client.post(
+            "/users",
+            json={
+                "nickname": "test_user",
+                "password": "Test_password",
+            },
+        )
+
+        response = client.post(
+            "/users/login",
+            json={
+                "nickname": "test_user",
+                "password": "Test_password",
+            },
+        )
+
+        response = client.post("/users/logout")
+
+        assert response.status_code == 200
+
+
+def test_logout_invalid_session_id():
+    with TestClient(app) as client:
+        client.post(
+            "/users",
+            json={
+                "nickname": "test_user",
+                "password": "Test_password",
+            },
+        )
+
+        response = client.post(
+            "/users/login",
+            json={
+                "nickname": "test_user",
+                "password": "Test_password",
+            },
+        )
+
+        client.cookies = {"session_id": "invalid_session_id"}
+        response = client.post("/users/logout")
+
+        assert response.status_code == 400
+        assert response.json()["detail"] == "존재하지 않는 세션입니다"
