3f299f188d7abd401f2f12351bebf8303742b470
ksg9482 <ksg930523@gmail.com>
2024-08-02 00:51:14 +0900

    Post모델과 Content모델 분리

--- src/domain/post.py
+++ src/domain/post.py
@@ -8,10 +8,20 @@ from sqlmodel import (
 from src.domain.user import User
 
 
+class Content(SQLModel, table=True):
+    id: int = Field(primary_key=True, index=True)
+    content: str
+    created_at: datetime = Field(default=func.now())
+    updated_at: datetime = Field(default_factory=func.now)
+
+    post: "Post" = Relationship(back_populates="content")
+
+
 class Post(SQLModel, table=True):
     id: int = Field(primary_key=True, index=True)
     title: str
-    content: str
+    content_id: int = Field(foreign_key="content.id")
+    content: Content = Relationship(back_populates="post")
     # default: 정의한 값으로 정적 채우기
     created_at: datetime = Field(default=func.now())
     # default_factory: 정의한 함수를 해당 시점에 호출해서 채우기
