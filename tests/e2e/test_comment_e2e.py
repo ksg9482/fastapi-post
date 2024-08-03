673cf513625340f2869159329cbcc30baaf176e3
ksg9482 <ksg930523@gmail.com>
2024-08-03 14:42:23 +0900

    admin 관련 처리 테스트 코드 추가

--- tests/e2e/test_comment_e2e.py
+++ tests/e2e/test_comment_e2e.py
@@ -41,6 +41,7 @@ def client():
         yield client
 
 
+@pytest.mark.create
 def test_create_comment_ok(client: TestClient):
     response = client.post(
         "/comments/1",
@@ -52,6 +53,7 @@ def test_create_comment_ok(client: TestClient):
     assert response.json()["content"] == "test_comment"
 
 
+@pytest.mark.create
 def test_create_comment_invalid_params(client: TestClient):
     response = client.post(
         url="/comments/1",
@@ -62,6 +64,7 @@ def test_create_comment_invalid_params(client: TestClient):
     assert response.status_code == 422
 
 
+@pytest.mark.create
 def test_create_comment_post_missing_field(client: TestClient):
     response = client.post(
         url="/comments/1",
@@ -72,6 +75,7 @@ def test_create_comment_post_missing_field(client: TestClient):
     assert response.status_code == 422
 
 
+@pytest.mark.create
 def test_create_comment_post_not_exists(client: TestClient):
     response = client.post(
         url="/comments/9999",
@@ -84,6 +88,7 @@ def test_create_comment_post_not_exists(client: TestClient):
 
 
 # 페이지네이션 해야함
+@pytest.mark.comments
 def test_comment_list_by_post_ok(client: TestClient):
     client.post(
         "/comments/1",
@@ -104,6 +109,7 @@ def test_comment_list_by_post_ok(client: TestClient):
     assert len(response.json()["comments"]) == 2
 
 
+@pytest.mark.comments
 def test_comment_list_by_post_empty_ok(client: TestClient):
     response = client.get("/comments/by_post/1")
 
@@ -112,6 +118,7 @@ def test_comment_list_by_post_empty_ok(client: TestClient):
 
 
 # 다른 유저가 넣은거 처리 해야함
+@pytest.mark.comments
 def test_comment_list_by_user_ok(client: TestClient):
     client.post(
         "/comments/1",
@@ -132,6 +139,7 @@ def test_comment_list_by_user_ok(client: TestClient):
     assert len(response.json()["comments"]) == 2
 
 
+@pytest.mark.comments
 def test_comment_list_by_user_empty_ok(client: TestClient):
     response = client.get("/comments/by_user/1")
 
@@ -147,6 +155,7 @@ def test_comment_list_by_user_empty_ok(client: TestClient):
 #     assert response.status_code == 500
 
 
+@pytest.mark.patch
 def test_comment_patch_ok(client: TestClient):
     client.post(
         url="/comments/1",
@@ -163,6 +172,7 @@ def test_comment_patch_ok(client: TestClient):
     assert comment_response.json()["comments"][0]["content"] == "test_comment_1_edit"
 
 
+@pytest.mark.patch
 def test_comment_patch_not_exists(client: TestClient):
     response = client.patch(url="/comments/1", json={"content": "test_comment_1_edit"})
 
@@ -170,6 +180,7 @@ def test_comment_patch_not_exists(client: TestClient):
     assert response.json()["detail"] == "존재하지 않는 코멘트입니다"
 
 
+@pytest.mark.patch
 def test_comment_patch_invalid_author(client: TestClient):
     client.post(
         url="/comments/1",
@@ -203,6 +214,35 @@ def test_comment_patch_invalid_author(client: TestClient):
     assert response.json()["detail"] == "작성자만 수정할 수 있습니다"
 
 
+@pytest.mark.patch
+def test_comment_patch_admin_role(client: TestClient):
+    client.post(
+        url="/comments/1",
+        json={
+            "content": "test_comment_1",
+        },
+    )
+
+    # admin role로 수정 시도. 세션아이디 변경 됨
+    client.post(
+        "/users",
+        json={"nickname": "test_user_2", "password": "Test_password", "role": "admin"},
+    )
+
+    client.post(
+        "/users/login",
+        json={
+            "nickname": "test_user_2",
+            "password": "Test_password",
+        },
+    )
+
+    response = client.patch(url="/comments/1", json={"content": "test_content_1_edit"})
+
+    assert response.status_code == 204
+
+
+@pytest.mark.delete
 def test_comment_delete_ok(client: TestClient):
     client.post(
         url="/comments/1",
@@ -219,13 +259,15 @@ def test_comment_delete_ok(client: TestClient):
     assert comment_response.json()["comments"] == []
 
 
-def test_comment_put_not_exists(client: TestClient):
+@pytest.mark.delete
+def test_comment_delete_not_exists(client: TestClient):
     response = client.delete(url="/comments/1")
 
     assert response.status_code == 400
     assert response.json()["detail"] == "존재하지 않는 코멘트입니다"
 
 
+@pytest.mark.delete
 def test_comment_delete_invalid_author(client: TestClient):
     client.post(
         "/comments/1",
@@ -257,3 +299,33 @@ def test_comment_delete_invalid_author(client: TestClient):
 
     assert response.status_code == 403
     assert response.json()["detail"] == "작성자만 삭제할 수 있습니다"
+
+
+@pytest.mark.delete
+def test_comment_delete_admin_role(client: TestClient):
+    client.post(
+        "/comments/1",
+        json={
+            "content": "test_comment_1",
+        },
+    )
+
+    # 다른 아이디로 삭제 시도. 세션아이디 변경 됨
+    client.post(
+        "/users",
+        json={"nickname": "test_user_2", "password": "Test_password", "role": "admin"},
+    )
+
+    client.post(
+        "/users/login",
+        json={
+            "nickname": "test_user_2",
+            "password": "Test_password",
+        },
+    )
+
+    # 쿠키에 이미 변경된 아이디가 들어있음. 명시로 다시 넣어주는게 좋은가?
+
+    response = client.delete(url="/comments/1")
+
+    assert response.status_code == 204
