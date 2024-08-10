01910c2c3eaf8959c6c71e0c52a91782bb1130c7
ksg9482 <ksg930523@gmail.com>
2024-08-10 23:31:00 +0900

    domains 객체와 속성 수정

--- src/domains/comment.py
+++ src/domains/comment.py
@@ -1,11 +1,12 @@
 from datetime import datetime
+from typing import Optional
 from sqlmodel import Field, SQLModel, Relationship, func
 from src.domains.user import User
 from src.domains.post import Post
 
 
 class Comment(SQLModel, table=True):
-    id: int = Field(primary_key=True, index=True)
+    id: Optional[int] = Field(primary_key=True)
     content: str
     created_at: datetime = Field(default=func.now())
     updated_at: datetime = Field(default_factory=func.now)
