95b3ac235cd2b11dad66dfa1e07f675c99029c10
ksg9482 <ksg930523@gmail.com>
2024-08-20 22:08:39 +0900

    테스트 DB세션 직접 연결

--- tests/e2e/test_user_e2e.py
+++ tests/e2e/test_user_e2e.py
@@ -1,3 +1,5 @@
+import os
+from dotenv import load_dotenv
 from httpx import ASGITransport, AsyncClient
 import pytest
 import pytest_asyncio
@@ -7,14 +9,16 @@ from sqlmodel.ext.asyncio.session import AsyncSession
 from sqlmodel import select
 
 from main import app
+from src.auth import hash_password
 from src.database import get_session
 from src.domains.user import User
 
 
-TEST_DATABASE_URL = "sqlite+aiosqlite:///:memory:"
+load_dotenv()
+TEST_DATABASE_URL = os.environ["DATABASE_URL"]
 
 
-@pytest_asyncio.fixture(scope="session")
+@pytest_asyncio.fixture(scope="function")
 async def test_db_init():
     test_engine = create_async_engine(url=TEST_DATABASE_URL, future=True)
     async with test_engine.begin() as conn:
@@ -53,8 +57,7 @@ def test_client(test_session: AsyncSession):
 @pytest.mark.asyncio
 @pytest.mark.create
 async def test_signup_user_ok(test_client: AsyncClient, test_session: AsyncSession):
-
-    # with test_client as client:
+    # when
     response = await test_client.post(
         "/",
         json={
@@ -62,6 +65,8 @@ async def test_signup_user_ok(test_client: AsyncClient, test_session: AsyncSessi
             "password": "Test_password",
         },
     )
+
+    # then
     assert response.status_code == 201
     assert response.json() == {"id": 1, "nickname": "test_user"}
 
@@ -70,168 +75,198 @@ async def test_signup_user_ok(test_client: AsyncClient, test_session: AsyncSessi
     assert user.nickname == "test_user"
 
 
-# @pytest.mark.create
-# def test_signup_user_not_upper():
-#     with TestClient(app) as client:
-#         response = client.post(
-#             "/users",
-#             json={
-#                 "nickname": "test_user",
-#                 "password": "test_password",
-#             },
-#         )
-#         assert response.status_code == 400
-#         assert (
-#             response.json()["detail"] == "문자열에 대문자가 하나 이상 포함되어야 합니다"
-#         )
+@pytest.mark.asyncio
+@pytest.mark.create
+async def test_signup_user_not_upper(
+    test_client: AsyncClient, test_session: AsyncSession
+):
+    # when
+    response = await test_client.post(
+        "/",
+        json={
+            "nickname": "test_user",
+            "password": "test_password",
+        },
+    )
+
+    # then
+    assert response.status_code == 400
+    assert response.json()["detail"] == "문자열에 대문자가 하나 이상 포함되어야 합니다"
+
+    result = await test_session.exec(select(User).where(User.nickname == "test_user"))
+    user = result.first()
+    assert user == None
 
 
-# @pytest.mark.create
-# def test_signup_user_invalid_params():
-#     with TestClient(app) as client:
-#         response = client.post(
-#             "/users",
-#             json={
-#                 "nickname": "",
-#                 "password": "",
-#             },
-#         )
-#         assert response.status_code == 422
+@pytest.mark.asyncio
+@pytest.mark.create
+async def test_signup_user_invalid_params(
+    test_client: AsyncClient, test_session: AsyncSession
+):
+    # when
+    response = await test_client.post(
+        "/",
+        json={
+            "nickname": "",
+            "password": "",
+        },
+    )
+
+    # then
+    assert response.status_code == 422
+
+    result = await test_session.exec(select(User).where(User.nickname == "test_user"))
+    user = result.first()
+    assert user == None
 
 
-# @pytest.mark.create
-# def test_signup_user_duplicate():
-#     with TestClient(app) as client:
-#         client.post(
-#             "/users",
-#             json={
-#                 "nickname": "test_user",
-#                 "password": "Test_password",
-#             },
-#         )
+@pytest.mark.asyncio
+@pytest.mark.create
+async def test_signup_user_duplicate(
+    test_client: AsyncClient, test_session: AsyncSession
+):
+    # given
+    hashed_password = hash_password(plain_password="Test_password")
+    new_user = User(nickname="test_user", password=hashed_password)
+    test_session.add(new_user)
+    await test_session.commit()
 
-#         response = client.post(
-#             "/users",
-#             json={
-#                 "nickname": "test_user",
-#                 "password": "Test_password",
-#             },
-#         )
-#         assert response.status_code == 409
-#         assert response.json()["detail"] == "이미 가입한 유저입니다"
+    # when
+    response = await test_client.post(
+        "/",
+        json={
+            "nickname": "test_user",
+            "password": "Test_password",
+        },
+    )
 
+    # then
+    assert response.status_code == 409
+    assert response.json()["detail"] == "이미 가입한 유저입니다"
 
-# @pytest.mark.login
-# def test_login_ok():
-#     with TestClient(app) as client:
-#         client.post(
-#             "/users",
-#             json={
-#                 "nickname": "test_user",
-#                 "password": "Test_password",
-#             },
-#         )
-
-#         response = client.post(
-#             "/users/login",
-#             json={
-#                 "nickname": "test_user",
-#                 "password": "Test_password",
-#             },
-#         )
-#         assert response.status_code == 200
-#         assert response.json()["session_id"]
+    result = await test_session.exec(select(User).where(User.nickname == "test_user"))
+    user = result.first()
+    assert user.nickname == "test_user"
 
 
-# @pytest.mark.login
-# def test_login_invalid_nikcname():
-#     with TestClient(app) as client:
-#         client.post(
-#             "/users",
-#             json={
-#                 "nickname": "test_user",
-#                 "password": "Test_password",
-#             },
-#         )
+@pytest.mark.asyncio
+@pytest.mark.login
+async def test_login_ok(test_client: AsyncClient, test_session: AsyncSession):
+    # given
+    hashed_password = hash_password(plain_password="Test_password")
+    new_user = User(nickname="test_user", password=hashed_password)
+    test_session.add(new_user)
+    await test_session.commit()
 
-#         response = client.post(
-#             "/users/login",
-#             json={
-#                 "nickname": "invalid_user",
-#                 "password": "Test_password",
-#             },
-#         )
-#         assert response.status_code == 401
-#         assert response.json()["detail"] == "존재하지 않는 유저입니다"
+    # when
+    response = await test_client.post(
+        "/login",
+        json={
+            "nickname": "test_user",
+            "password": "Test_password",
+        },
+    )
 
-
-# @pytest.mark.login
-# def test_login_invalid_password():
-#     with TestClient(app) as client:
-#         client.post(
-#             "/users",
-#             json={
-#                 "nickname": "test_user",
-#                 "password": "Test_password",
-#             },
-#         )
-
-#         response = client.post(
-#             "/users/login",
-#             json={
-#                 "nickname": "test_user",
-#                 "password": "invalid_password",
-#             },
-#         )
-#         assert response.status_code == 401
-#         assert response.json()["detail"] == "잘못된 비밀번호입니다"
+    # then
+    assert response.status_code == 200
+    assert response.json()["session_id"]
 
 
-# @pytest.mark.logout
-# def test_logout_ok():
-#     with TestClient(app) as client:
-#         client.post(
-#             "/users",
-#             json={
-#                 "nickname": "test_user",
-#                 "password": "Test_password",
-#             },
-#         )
+@pytest.mark.asyncio
+@pytest.mark.login
+async def test_login_invalid_nikcname(
+    test_client: AsyncClient, test_session: AsyncSession
+):
+    # given
+    hashed_password = hash_password(plain_password="Test_password")
+    new_user = User(nickname="test_user", password=hashed_password)
+    test_session.add(new_user)
+    await test_session.commit()
 
-#         response = client.post(
-#             "/users/login",
-#             json={
-#                 "nickname": "test_user",
-#                 "password": "Test_password",
-#             },
-#         )
+    # when
+    response = await test_client.post(
+        "/login",
+        json={
+            "nickname": "invalid_user",
+            "password": "Test_password",
+        },
+    )
 
-#         response = client.post("/users/logout")
+    # then
+    assert response.status_code == 401
+    assert response.json()["detail"] == "존재하지 않는 유저입니다"
 
-#         assert response.status_code == 200
+
+@pytest.mark.asyncio
+@pytest.mark.login
+async def test_login_invalid_password(
+    test_client: AsyncClient, test_session: AsyncSession
+):
+    # given
+    hashed_password = hash_password(plain_password="Test_password")
+    new_user = User(nickname="test_user", password=hashed_password)
+    test_session.add(new_user)
+    await test_session.commit()
+
+    # when
+    response = await test_client.post(
+        "/login",
+        json={
+            "nickname": "test_user",
+            "password": "invalid_password",
+        },
+    )
+
+    # then
+    assert response.status_code == 401
+    assert response.json()["detail"] == "잘못된 비밀번호입니다"
 
 
-# @pytest.mark.logout
-# def test_logout_invalid_session_id():
-#     with TestClient(app) as client:
-#         client.post(
-#             "/users",
-#             json={
-#                 "nickname": "test_user",
-#                 "password": "Test_password",
-#             },
-#         )
+@pytest.mark.asyncio
+@pytest.mark.logout
+async def test_logout_ok(test_client: AsyncClient, test_session: AsyncSession):
+    # given
+    hashed_password = hash_password(plain_password="Test_password")
+    new_user = User(nickname="test_user", password=hashed_password)
+    test_session.add(new_user)
+    await test_session.commit()
 
-#         response = client.post(
-#             "/users/login",
-#             json={
-#                 "nickname": "test_user",
-#                 "password": "Test_password",
-#             },
-#         )
+    # when
+    await test_client.post(
+        "/login",
+        json={
+            "nickname": "test_user",
+            "password": "Test_password",
+        },
+    )
+    response = await test_client.post("/logout")
 
-#         client.cookies = {"session_id": "invalid_session_id"}
-#         response = client.post("/users/logout")
+    # then
+    assert response.status_code == 200
 
-#         assert response.status_code == 400
-#         assert response.json()["detail"] == "존재하지 않는 세션입니다"
+
+@pytest.mark.asyncio
+@pytest.mark.logout
+async def test_logout_invalid_session_id(
+    test_client: AsyncClient, test_session: AsyncSession
+):
+    # given
+    hashed_password = hash_password(plain_password="Test_password")
+    new_user = User(nickname="test_user", password=hashed_password)
+    test_session.add(new_user)
+    await test_session.commit()
+
+    # when
+    response = await test_client.post(
+        "/login",
+        json={
+            "nickname": "test_user",
+            "password": "Test_password",
+        },
+    )
+    test_client.cookies = {"session_id": "invalid_session_id"}
+    response = await test_client.post("/logout")
+
+    # then
+    assert response.status_code == 400
+    assert response.json()["detail"] == "존재하지 않는 세션입니다"
