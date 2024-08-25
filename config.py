156ddeb0b6e7f7c193f09555ab67ea14a78d467a
ksg9482 <ksg930523@gmail.com>
2024-08-26 00:26:10 +0900

    환경변수 주입 padantic-setting도입

---
+++ config.py
@@ -0,0 +1,7 @@
+from pydantic import Field
+from pydantic_settings import BaseSettings
+
+
+class Config(BaseSettings):
+    DATABASE_URL: str = Field(default="sqlite+aiosqlite:///:memory:")
+    TEST_DATABASE_URL: str = Field(default="sqlite+aiosqlite:///:memory:")
