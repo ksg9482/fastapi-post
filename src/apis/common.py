213dd9010b5b27dcf561b5b1a816a87f8e5010eb
ksg9482 <ksg930523@gmail.com>
2024-08-26 00:26:29 +0900

    locust 도입

---
+++ src/apis/common.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter, status
+
+router = APIRouter(tags=["common"])
+
+
+@router.get(
+    "/",
+    status_code=status.HTTP_200_OK,
+)
+async def health() -> None:
+    # health check
+    return
