1af88d131936a1062f3b4a0fdf321922e1c59baa
ydarh <110903858+f-lab-ydrah@users.noreply.github.com>
2024-09-20 11:18:32 +0900

    Update config.py

--- src/config.py
+++ src/config.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings
 
 
 class Config(BaseSettings):
+    # TODO: 아래 변수 값이 DATABASE_URL 환경 변수를 읽어 오나요? 잘 읽어오는지 확인해보고 수정할 부분 있으면 수정해보세요.
     DATABASE_URL: str = Field(default="sqlite+aiosqlite:///:memory:")
     REDIS_URL: str = Field(default="redis://localhost")
 
